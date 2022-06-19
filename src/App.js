d042a0d5cb0b53cd9008e071a15466a075b1b2cb
Randall Holsinger <randallholsinger@gmail.com>
2022-06-18 17:17:54 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jestjs.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Jest and React-Testing-Library
         </a>
       </header>
     </div>
